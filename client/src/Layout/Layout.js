e973496265ba71ad22cb2034f2de18f1555bbf4c
Manik-Mittal <131521271+Manik-Mittal@users.noreply.github.com>
2024-06-26 17:59:54 +0530

    adds about us , contact us functionality

--- client/src/Layout/Layout.js
+++ client/src/Layout/Layout.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import Header from "./Header";
 import Footer from './Footer';
-
-
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
     <div>
-      <Header/>
-      <main style={{minHeight:'84vh'}}>
-      <ToastContainer />
-      {children}
+      <Header />
+      <main style={{ minHeight: '84vh' }}>
+        <ToastContainer />
+
+        {children}
 
 
       </main>
---
+++ client/src/pages/Privacy.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+const Privacy = () => {
+  return (
+    <Layout title={"Privacy Policy"}>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/privacypol.jpg"
+            alt="contactus"
+            style={{ width: "100%" }}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>We use your data</p>
+          <p>We use your data</p>
+          <p>We use your data</p>
+          <p>We use your data</p>
+          <p>We use your data</p>
+          <p>We use your data</p>
+          <p>We use your data</p>
+
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
+
+export default Privacy
